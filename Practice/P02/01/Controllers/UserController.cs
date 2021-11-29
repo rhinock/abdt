feb5ad28e3d6274a8cb659e025c282d644c2b410
rhinock <rhinock@yandex.ru>
2021-11-29 16:06:20 +0300

    P02 - init

---
+++ Practice/P02/01/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using _01.Enums;
+using _01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace _01.Controllers
+{
+
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private static Dictionary<long, User> Users = new()
+        {
+            [1] = new User { Id = 1, Name = "Owner", Role = Role.Admin }
+        };
+
+        /// <summary>
+        /// Get user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="404">User not found</response>
+        /// <returns></returns>
+        [HttpGet("{id:long}")]
+        [ProducesResponseType(typeof(User), 200)]
+        [ProducesResponseType(typeof(ApiError), 404)]
+        public ActionResult<User> Get(long id)
+        {
+            if (!Users.ContainsKey(id))
+                return NotFound(new ApiError() { Code = 1, Message = "User not found" });
+
+            return Ok(Users[id]);
+        }
+
+        /// <summary>
+        /// Create user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <response code="400">Invalid role</response>
+        /// <response code="409">User already exists</response>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post(User user)
+        {
+            if (!Enum.IsDefined(user.Role))
+                return BadRequest(new ApiError() { Code = 2, Message = "Invalid role" });
+
+            if (Users.ContainsKey(user.Id))
+                return Conflict(new ApiError { Code = 3, Message = "User already exist" });
+
+            Users[user.Id] = user;
+            return Ok();
+        }
+
+        /// <summary>
+        /// Create user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <response code="400">Invalid role</response>
+        /// <response code="409">User already exists</response>
+        /// <returns></returns>
+        [Obsolete]
+        [HttpPost("legacy")]
+        public ActionResult PostLegacy(User user)
+        {
+            Users[user.Id] = user;
+            return Ok();
+        }
+    }
+}
---
+++ Practice/P02/01/Enums/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace _01.Enums
+{
+    /// <summary>
+    /// User role
+    /// </summary>
+    public enum Role
+    {
+        Guest = 0,
+        Operators,
+        Admin
+    }
+}
