0edc2d64ee00ab2d2497a2631428b2c527765ea0
rhinock <rhinock@yandex.ru>
2021-11-30 14:39:44 +0300

    10

---
+++ Practice/P03/10/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using _10.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace _10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<TestConfig> _config;
+
+        public HomeController(IOptions<TestConfig> config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(_config);
+        }
+    }
+}
---
+++ Practice/P03/10/Startup.cs
@@ -0,0 +1,45 @@
+using _10.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _10
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.Configure<TestConfig>(Configuration.GetSection("TestConfig"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
