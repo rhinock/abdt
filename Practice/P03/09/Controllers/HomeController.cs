92c83002a55d164b4563683582e8ea147cf9bcf7
rhinock <rhinock@yandex.ru>
2021-11-30 14:21:18 +0300

    09

---
+++ Practice/P03/09/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using _09.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _09.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(EditViewModel model)
+        {
+            return View();
+        }
+    }
+}
