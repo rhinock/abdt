5211dffc556f65720795cdfdd6b3086e07eb5115
rhinock <rhinock@yandex.ru>
2021-11-30 16:13:22 +0300

    11

---
+++ Practice/P03/11/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11.Models
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+
+        public string Cvc { get; set; }
+
+        public string Pan { get; set; }
+
+        public Expire Expire { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
