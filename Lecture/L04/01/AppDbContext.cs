e7e99686ed0312099fb735c75540a77ef00f9116
rhinock <rhinock@yandex.ru>
2021-12-05 16:09:27 +0300

    L04 - 01

---
+++ Lecture/L04/01/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using _01.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _01
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Account>(x =>
+            {
+                x.HasKey(s => s.Id);
+                x.Property(s => s.AccountNumber).HasMaxLength(30);
+                x.HasOne(s => s.Card)
+                    .WithOne();
+            });
+        }
+    }
+}
---
+++ Lecture/L04/01/Entities/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.Entities
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string AccountNumber { get; set; }
+        public long CardId { get; set; }
+        public Card Card { get; set; }
+    }
+}
